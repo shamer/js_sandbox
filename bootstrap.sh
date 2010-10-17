646e4e71df46b73f7f49e62f8471374024d6e9f4
Stephen Hamer <stephen.hamer@gmail.com>
2010-10-16 19:03:23 -0700

    Updated bootstrap script and gitignore for envjs.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+git submodule update --init
+
 svn checkout http://closure-linter.googlecode.com/svn tools/closure-linter
+
+tar xzf tools/thatcher-env-js-1.2.13-0-gcb738b9.tar.gz -C tools
+mv tools/thatcher-env-js-cb738b9 tools/envjs
+cd tools/envjs
+make
 
 echo "run 'easy_install closure_linter' from the tools directory to install gjslint"
